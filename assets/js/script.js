a96f6559da1ba7f29f64f1f4c3ecb348dd0a562f
Owais Islam <owaisislam@gmail.com>
2020-08-30 19:19:48 -0500

    added additional checks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,14 @@ var future_task_color = 'rgb(173,255,47,0.8)';
 var currentDate = moment().format('dddd, MMMM Do');
 $("#currentDay").text(currentDate);
 
+var tasks = JSON.parse(localStorage.getItem("tasks"));
 var timeSlots = $(".to-do").toArray();
 
-var tasks = JSON.parse(localStorage.getItem("tasks"));
 
 function setTimeSlotColors() {
     var currentTime = moment().format('kk');
     for (var i = 0; i < timeSlots.length; i++) {
-        if (parseInt(timeSlots[i].id) == currentTime) {
+        if (parseInt(timeSlots[i].id) == (currentTime - 4)) {
             timeSlots[i].style.backgroundColor = current_task_color;
         } else if (parseInt(timeSlots[i].id) > currentTime) {
             timeSlots[i].style.backgroundColor = future_task_color;
@@ -24,12 +24,25 @@ function getStoredTasks() {
         tasks = [];
     } else {
         tasks = JSON.parse(localStorage.getItem("tasks"));
-        console.log(tasks);
     }
+
+    $(".display-text").each(function (index, element) {
+        for (var i = 0; i < tasks.length; i++) {
+            if (tasks[i].pID == element.id) {
+                element.textContent = tasks[i].task;
+            }
+        }
+    })
 }
 
 
 $(".to-do").on("click", function () {
+    $(".save-button").each(function (index, element) {
+        element.style.opacity = 0.2;
+    })
+
+    $(this).parent().children(".save-button")[0].style.opacity = 1;
+
     $(".form-control").each(function (index, element) {
         element.style.visibility = "hidden";
     })
@@ -39,6 +52,11 @@ $(".to-do").on("click", function () {
 });
 
 $(".save-button").on("click", function () {
+    $(".save-button").each(function (index, element) {
+        element.style.opacity = (0.7);
+    })
+
+
     var currentTaskForm = $(this).parent().children(".to-do").children(".form-control")[0];
 
     var currentP = $(this).parent().children(".to-do").children(".display-text")[0];
@@ -48,11 +66,16 @@ $(".save-button").on("click", function () {
     currentTaskForm.style.visibility = "hidden";
 
     var currentTask = {
-        "p-id": 4,
+        "pID": currentP.id,
         "task": taskDescription
     }
     tasks.push(currentTask);
     localStorage.setItem("tasks", JSON.stringify(tasks));
+});
+
+$("#remove-tasks").on("click", function () {
+    localStorage.clear();
+    location.reload();
 });
 
 
